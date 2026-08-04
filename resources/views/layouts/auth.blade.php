f11f6e11f4ec5f98044df4976c7441e630ddf094
AlejandrouUmgGt <dvasquezy3@miumg.edu.gt>
2026-08-04 06:25:17 +0000

    inicial

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.base')
+@section('content')
+    <main class="max-w-2xl mt-10 mx-auto p-10 shadow-lg rounded-lg bg-white border border-gray-200">
+        @yield('auth-content')
+    </main>
+@endsection
